75f9869f27f2599c06346cf495b48c06ac4905b6
Marko Babović <mb20170119@student.fon.bg.ac.rs>
2021-05-09 19:00:15 +0200

    Uradjena validacija

--- src/app/main/autf/login/login.page.ts
+++ src/app/main/autf/login/login.page.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../interfaces/user.model';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,40 @@ import {User} from '../../interfaces/user.model';
 export class LoginPage implements OnInit {
 
 
-  constructor() {
+  validationFormUser: FormGroup;
+  validationUserMessage ={
+    email:[
+      {type:'required', message:'Please enter your Email !'},
+      {type:'pattern', message:'The email entered is incorrect. Try again'}
+    ],
+    password:[
+      {type:'required', message:'Please enter your password !'},
+      {type:'minlength', message:'The password must be at least 6 characters or more'}
+
+    ]
+  };
+  constructor(public formBuilder: FormBuilder) {
 
 
 
   }
+
 
   ngOnInit(){
+    this.validationFormUser = this.formBuilder.group({
+      email: new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ]))
+    });
   }
 
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  LoginUser(value: any) {
+
+  }
 }
--- src/app/main/autf/sign-up/sign-up.page.ts
+++ src/app/main/autf/sign-up/sign-up.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  validationMessages = {
+    username: [{type:'required', message:'Please enter username'}],
+    email: [
+      {type: 'required',message:'Enter your email'},
+      {type:'pattern', message:'The Email entered is incorrect. Try again.'}
+    ],
+    password: [
+      {type: 'required', message: 'Password is required here'},
+      {type:'minlength', message: 'Password must be at least 6 character'}
+    ]
+  };
+  validationFormUser: FormGroup;
+  constructor(private formBuilder: FormBuilder ) {
+
+      }
+
+
 
   ngOnInit() {
+    this.validationFormUser = this.formBuilder.group({
+      username: new FormControl('',Validators.compose([
+        Validators.required
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ]))
+    });
   }
 
+  registerUser(value: any) {
+
+  }
 }
