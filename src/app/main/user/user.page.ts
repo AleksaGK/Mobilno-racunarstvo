5340f71e6f6594b9670ba23146f52cb71505efa8
Marko Babović <mb20170119@student.fon.bg.ac.rs>
2021-05-10 21:21:16 +0200

    Napravljen user dio, ubacen alert, koji radi

--- src/app/main/user/user.page.ts
+++ src/app/main/user/user.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../autf/service/auth.service';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +10,43 @@ import {AuthService} from '../autf/service/auth.service';
 export class UserPage implements OnInit {
 
   logged: boolean;
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private alertController: AlertController) { }
 
 
   ngOnInit() {
+  }
+  ionViewWillEnter(){
+
+    console.log('cao');
     this.logged = this.authService.isLoggedIn;
   }
 
 
+  onAlert() {
+
+    this.alertController.create({
+      cssClass: 'alertHeader',
+      header: 'Log out',
+      message: 'Are you sure you want to log out ?',
+      buttons:[
+        {
+          text: 'Yes',
+          handler:()=>{
+            console.log('Yes');
+            this.authService.isLoggedIn=false;
+            this.logged= false;
+          }
+        },
+        {
+          text: 'No',
+          role: 'Cancel',
+          handler:()=>{
+            console.log('No');
+          }
+        }
+      ]
+    }).then((alert)=>{
+      alert.present();
+    });
+  }
 }
