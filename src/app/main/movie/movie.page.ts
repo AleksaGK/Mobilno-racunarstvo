50df0b9e52544a25fc9fa7c6020c3eb2649cdcfc
Marko Babović <mb20170119@student.fon.bg.ac.rs>
2021-05-11 16:13:56 +0200

    Napravljen template za rate filmova

--- src/app/main/movie/movie.page.ts
+++ src/app/main/movie/movie.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../interfaces/movie.model';
 import { MainService } from '../main.service';
+import {ModalController} from '@ionic/angular';
+import {MovieModalComponent} from './movie-modal/movie-modal.component';
 
 @Component({
   selector: 'app-movie',
@@ -21,15 +23,25 @@ export class MoviePage implements OnInit {
       modifier: 1,
       slideShadows: true,
     }
-  }
+  };
 
-  constructor(private route: ActivatedRoute, private service: MainService) { }
+  constructor(private route: ActivatedRoute, private service: MainService, private modalController: ModalController) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
       this.movie = this.service.getMovie(paramMap.get('MovieId'));
-    })
+    });
     // console.log(this.movie.MovieId)
   }
 
+  openModal() {
+this.modalController.create({
+  component: MovieModalComponent,
+  componentProps:{
+    movie:this.movie
+  }
+}).then((modal)=>{
+  modal.present();
+});
+  }
 }
