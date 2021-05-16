36241a9ed43a4e1a8d4ca8657c3ab844e83e4a2a
Marko Babović <mb20170119@student.fon.bg.ac.rs>
2021-05-16 23:04:50 +0200

    Auth commit

--- src/app/main/autf/service/auth.service.ts
+++ src/app/main/autf/service/auth.service.ts
@@ -6,28 +6,15 @@ import {User} from '../../interfaces/user.model';
   providedIn: 'root'
 })
 export class AuthService {
-   private _isLoggedIn = true;
-   private url = 'http://localhost:16615/api/user';
 
-   get isLoggedIn(): boolean {
-    return this._isLoggedIn;
-   }
-
-  set isLoggedIn(value: boolean) {
-    this._isLoggedIn = value;
-  }
-
-  public user: User;
-
+    private url = 'http://localhost:16615/api/user';
+    public user: User;
 
   constructor( private http: HttpClient) {
 
   }
 
   registerUser(value: any) {
-
-    // const headers: HttpHeaders = new  HttpHeaders();
-    // headers.set('Content-Type', 'application/json');
 
     const  data = {username: value.username, password: value.password,email: value.email};
 
